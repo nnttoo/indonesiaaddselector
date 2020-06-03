75598ffa72a24fd9f5941304b0e2e4dc9fd94d47
Haryanto <gendang_telinga@yahoo.com>
2020-06-04 06:38:22 +0800

    change to responsive

--- src/expressrun.js
+++ src/expressrun.js
@@ -3,5 +3,5 @@ const express = require('express')
 const expApp = express() 
 
 expApp.use('/',express.static( 'docs'))
-expApp.listen("8080")
-console.log("http://127.0.0.1:8080")
+expApp.listen("9090",'0.0.0.0')
+console.log("http://127.0.0.1:9090")
